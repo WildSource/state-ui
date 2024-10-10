54c51babf02c8a6ec6386ecade2ef9bd983e17f3
WildSource <ilovetrap123@hotmail.com>
2024-10-09 21:49:21 -0400

    untested html tag generation

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,9 +1,33 @@
 module MyLib where
 
 import System.Directory
+import qualified Data.Text as T
+import Data.Text (Text)
 
 type Path = String
+type TagName = T.Text 
+
+tag :: TagName -> T.Text 
+tag name = 
+  let oTag = T.pack "<"
+      cTag = T.pack ">"
+  in oTag <> name <> cTag 
+
+endTag :: TagName -> T.Text 
+endTag name = 
+  let oTag = T.pack "<"
+      cTag = T.pack "/>"
+  in oTag <> name <> cTag 
+
+tags :: TagName -> T.Text
+tags name = tag name <> endTag name
+
+writeTemplate :: Path -> IO ()
+writeTemplate = 
+  let content = T.pack "html"
+  in flip (writeFile) (T.unpack $ tags content) 
 
 writeProject :: Path -> IO ()
 writeProject path = do
   createDirectory path
+  writeTemplate (path ++ "/index.html") 
