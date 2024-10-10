3d432aeae5ff41676fc14bc43845862898525e31
WildSource <ilovetrap123@hotmail.com>
2024-10-10 07:52:24 -0400

    add newline operator

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -7,6 +7,15 @@ import Data.Text (Text)
 type Path = String
 type TagName = T.Text 
 
+data Tag = Tag {
+  html :: T.Text  
+}
+
+(<#>) :: T.Text -> T.Text -> T.Text
+front <#> back = 
+  let newline = T.pack "\n"
+  in  front <> newline <> back
+
 tag :: TagName -> T.Text 
 tag name = 
   let oTag = T.pack "<"
@@ -21,6 +30,9 @@ endTag name =
 
 tags :: TagName -> T.Text
 tags name = tag name <> endTag name
+
+template :: Tag
+template = Tag $ tag (T.pack "!DOCTYPE html")
 
 writeTemplate :: Path -> IO ()
 writeTemplate = 
